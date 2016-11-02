094e8741bdd57864d2d69aee17cd54fec6d7ddd5
Zseng <Zsengofmax@gmail.com>
2016-11-02 14:40:14 +0800

    refresh container height

--- WebRoot/js/index.js
+++ WebRoot/js/index.js
@@ -4,6 +4,18 @@
 var btn = document.querySelector('.loginPage');
 var container = document.querySelector('.container');
 var background = document.querySelector('.background');
+
+var pageContainer = document.querySelector('.page-container');
+
+//更新容器高度,填满浏览器
+var height = document.body.clientHeight ? document.body.clientHeight : document.documentElement.clientHeight;
+container.style.height = height + "px";
+background.style.height = height + "px";
+pageContainer.style.height = height + "px";
+
 btn.addEventListener('click', function () {
-    $('.background').animate({marginTop: "-780px"}, 700);
+    $('.background').animate({marginTop: "-" + height + "px"}, 700);
 }, false);
+
+
+//更新容器高度,填满浏览器
