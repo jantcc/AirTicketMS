f1549e9dedaa2f6ba8694729fab169230a10025b
Zseng <Zsengofmax@gmail.com>
2016-11-02 10:30:43 +0800

    add index page

--- WebRoot/js/index.js
+++ WebRoot/js/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by Max on 16/11/2.
  */
+var btn = document.querySelector('.loginPage');
+var container = document.querySelector('.container');
+var background = document.querySelector('.background');
+btn.addEventListener('click', function () {
+    $('.background').animate({marginTop: "-738px"}, 700);
+}, false);
