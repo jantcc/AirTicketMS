fb6bcaa0d2b37edd330000c88e6e8e800b277dc3
Zseng <Zsengofmax@gmail.com>
2016-11-02 10:34:07 +0800

    update

--- WebRoot/js/index.js
+++ WebRoot/js/index.js
@@ -5,5 +5,5 @@ var btn = document.querySelector('.loginPage');
 var container = document.querySelector('.container');
 var background = document.querySelector('.background');
 btn.addEventListener('click', function () {
-    $('.background').animate({marginTop: "-738px"}, 700);
+    $('.background').animate({marginTop: "-780px"}, 700);
 }, false);
